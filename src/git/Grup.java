279b82347e9bf367a3596323cc817bb7b11091ba
MerveOkur <74603337+MerveOkur@users.noreply.github.com>
2020-11-26 15:46:06 +0300

    Update Grup.java

--- src/git/Grup.java
+++ src/git/Grup.java
@@ -8,6 +8,9 @@ public class Grup {
       System.out.println("15:00 - ikinci commit");
       System.out.println("15:07 - ucuncu commit");
       System.out.println("15:19 - dorduncu commit");
+		
+		
+     System.out.println("15:45 - son commit");
 	}
 
 }
